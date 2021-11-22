6b4932694200946ba9cb12061b6c45bc82ffd471
Topaz <topazeinav@gmail.com>
2021-11-22 17:58:02 +0200

    first snapshot

---
+++ x_and_o.py
@@ -0,0 +1,103 @@
+# v0.1 by TOPAZ
+board = [[' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']]
+
+
+
+def thisprintstheboard():
+    print(board[0])
+    print(board[1])
+    print(board[2])
+thisprintstheboard()
+print("player_1 what column")
+user_input_column =input()
+print("player_1 what line")
+user_input_line=input()
+if user_input_column == "1" and user_input_line == "1" :
+    board[0][0]="x"
+elif user_input_column =="1" and user_input_line =="2":
+    board[1][0]="x"
+elif user_input_column =="1" and user_input_line =="3":
+    board[2][0]="x"
+elif user_input_column =="2" and user_input_line =="1":
+    board[0][1]="x"
+elif user_input_column =="2" and user_input_line =="2":
+    board[1][1]="x"
+elif user_input_column =="2" and user_input_line =="3":
+    board[2][1]="x"
+elif user_input_column =="3" and user_input_line =="1":
+    board[0][2]="x"
+elif user_input_column =="3" and user_input_line =="2":
+    board[1][2]="x"
+elif user_input_column =="3" and user_input_line =="3":
+    board[2][2]="x"
+else:
+    print("why do you try to break me i am not dumb y'know")
+    board [0][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+    board [2][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+    board [1][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+
+
+thisprintstheboard()
+
+print("player_2 what column")
+user_input_column =input()
+print("player_2 what line")
+user_input_line=input()
+if user_input_column == "1" and user_input_line == "1" :
+    board[0][0]="o"
+elif user_input_column =="1" and user_input_line =="2":
+    board[1][0]="o"
+elif user_input_column =="1" and user_input_line =="3":
+    board[2][0]="o"
+elif user_input_column =="2" and user_input_line =="1":
+    board[0][1]="o"
+elif user_input_column =="2" and user_input_line =="2":
+    board[1][1]="o"
+elif user_input_column =="2" and user_input_line =="3":
+    board[2][1]="o"
+elif user_input_column =="3" and user_input_line =="1":
+    board[0][2]="o"
+elif user_input_column =="3" and user_input_line =="2":
+    board[1][2]="o"
+elif user_input_column =="3" and user_input_line =="3":
+    board[2][2]="o"
+else:
+    print("why do you try to break me i am not dumb y'know")
+    board [0][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+    board [2][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+    board [1][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+
+thisprintstheboard()
+
+
+print("player_1 what column")
+user_input_column =input()
+print("player_1 what line")
+user_input_line=input()
+if user_input_column == "1" and user_input_line == "1" :
+    board[0][0]="x"
+elif user_input_column =="1" and user_input_line =="2":
+    board[1][0]="x"
+elif user_input_column =="1" and user_input_line =="3":
+    board[2][0]="x"
+elif user_input_column =="2" and user_input_line =="1":
+    board[0][1]="x"
+elif user_input_column =="2" and user_input_line =="2":
+    board[1][1]="x"
+elif user_input_column =="2" and user_input_line =="3":
+    board[2][1]="x"
+elif user_input_column =="3" and user_input_line =="1":
+    board[0][2]="x"
+elif user_input_column =="3" and user_input_line =="2":
+    board[1][2]="x"
+elif user_input_column =="3" and user_input_line =="3":
+    board[2][2]="x"
+else:
+    print("why do you try to break me i am not dumb y'know")
+    board [0][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+    board [2][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+    board [1][1]="AHHHHHHHHHHH CODE RED IM BREAKING"
+
+thisprintstheboard()
